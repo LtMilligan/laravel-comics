c356d8869d13ea2b60fb7cb8a0ee4ff857b94d90
LtMilligan <gioele.miscia@gmail.com>
2024-09-24 19:50:02 +0200

    [UPD] Complete main content with style

--- resources/views/layout/partials/main.blade.php
+++ resources/views/layout/partials/main.blade.php
@@ -5,14 +5,16 @@
     <div class="bg-grey p-5">
         <div class="container">
             <div class="row content-grey d-flex">
-                <div class="col-2 d-flex" v-for="(comic, index) in comics" :key="index">
-                    <div class="card border-0 bg-transparent w-100 mx-3">
-                        <img :src="comic.thumb" :alt="comic.series" class="card-img-top img-fluid">
-                        <div>
-                            <h5 class="text-white text-center pt-2"></h5>
+                @foreach ($comics as $comic)
+                    <div class="col-2 d-flex">
+                        <div class="card border-0 bg-transparent w-100 mx-3">
+                            <img src="{{ $comic['thumb'] }}" alt="{{ $comic['title'] }}" class="card-img-top img-fluid">
+                            <div>
+                                <h5 class="text-white text-center pt-2">{{ $comic['series'] }}</h5>
+                            </div>
                         </div>
                     </div>
-                </div>
+                @endforeach
                 <div class="text-center">
                     <button class="btn btn-primary w-auto px-5 rounded-0"><strong>LOAD MORE</strong></button>
                 </div>
